ea08b221c8b9d07a07791b33c3c996a676da3472
Nic-Horton <77477203+Nic-Horton@users.noreply.github.com>
2023-04-21 23:26:47 -0500

    Create feature to display book info via a card on the screen

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 let myLibrary = [];
 let newBook;
 
+// const submitBtn = document.querySelector('#submit');
+// submitBtn.addEventListener('click', () => {
+// 	addToLibrary();
+// });
+
 function Book(title, author, pages, read) {
 	this.title = title;
 	this.author = author;
-	this.pages = pages;
+	this.pages = pages + 'pgs';
 	this.read = read;
 }
 
@@ -16,14 +21,69 @@ function addToLibrary() {
 	let bookRead = document.getElementById('read').checked;
 	newBook = new Book(bookTitle, bookAuthor, bookPages, bookRead);
 	myLibrary.push(newBook);
+	display();
 	form.reset();
 	closeForm();
 }
 
 function display() {
+	const bookShelf = document.getElementById('bookShelf');
+	const books = document.querySelectorAll('.card');
+	books.forEach((card) => bookShelf.removeChild(card));
 	for (i = 0; i < myLibrary.length; i++) {
-		console.log(myLibrary[i]);
+		createCard(myLibrary[i]);
 	}
+}
+
+function createCard(book) {
+	const library = document.querySelector('#bookShelf');
+	const cardDiv = document.createElement('div');
+	const titleDiv = document.createElement('div');
+	const authorDiv = document.createElement('div');
+	const pagesDiv = document.createElement('div');
+	const readBtn = document.createElement('button');
+	const removeBtn = document.createElement('button');
+
+	cardDiv.classList.add('card');
+	cardDiv.setAttribute('id', myLibrary.indexOf(book));
+
+	titleDiv.textContent = book.title;
+	titleDiv.classList.add('title');
+	cardDiv.appendChild(titleDiv);
+
+	authorDiv.textContent = book.author;
+	authorDiv.classList.add('author');
+	cardDiv.appendChild(authorDiv);
+
+	pagesDiv.textContent = book.pages;
+	pagesDiv.classList.add('pages');
+	cardDiv.appendChild(pagesDiv);
+
+	readBtn.classList.add('readBtn');
+	cardDiv.appendChild(readBtn);
+	if (book.read === false) {
+		readBtn.textContent = 'Not Read';
+		readBtn.style.backgroundColor = '#e04f63';
+	} else {
+		readBtn.textContent = 'Read';
+		readBtn.style.backgroundColor = '#63da63';
+	}
+
+	removeBtn.textContent = 'Remove';
+	removeBtn.setAttribute('id', 'removeBtn');
+	cardDiv.appendChild(removeBtn);
+
+	library.appendChild(cardDiv);
+
+	removeBtn.addEventListener('click', () => {
+		myLibrary.splice(myLibrary.indexOf(book), 1);
+		display();
+	});
+
+	readBtn.addEventListener('click', () => {
+		book.read = !book.read;
+		display();
+	});
 }
 
 const bookShelf = document.getElementsByClassName('bookShelf');
